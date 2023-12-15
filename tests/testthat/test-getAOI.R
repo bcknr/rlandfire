2890354af739fa45df4721bb0dbe26547d809ea6
bcknr <mab677@cornell.edu>
2023-12-15 10:36:18 -0500

    Updated tests for all functions

--- tests/testthat/test-getAOI.R
+++ tests/testthat/test-getAOI.R
@@ -16,7 +16,7 @@ test_that("`getAOI` works with supported object classes", {
 })
 
 
-test_that("`landfireAPI()` recognizes arguement errors", {
+test_that("`getAOI()` recognizes arguement errors", {
   r <- terra::rast(nrows = 50, ncols = 50,
                    xmin = -123.7835, xmax = -123.6352,
                    ymin = 41.7534, ymax = 41.8042,
@@ -24,11 +24,11 @@ test_that("`landfireAPI()` recognizes arguement errors", {
                    vals = rnorm(2500))
 
   # Check for required arguments
-  expect_error(landfireAPI(aoi = aoi),
-               "argument `products` is missing with no default")
+  expect_error(getAOI(extend = c(10,15)),
+               'argument "data" is missing, with no default')
 
-  expect_error(landfireAPI(products),
-               "argument `aoi` is missing with no default")
+  expect_error(getAOI(data = r, extend = c(10,15), sf_order = 1),
+               "argument `sf_order` must be logical")
 
   # Check class
   expect_error(getAOI(r, extend = "string"),
@@ -68,4 +68,53 @@ test_that("`getAOI` returns correct CRS", {
 
   # Expect minor differences from transformation
   expect_equal(getAOI(r), ext, tolerance = 0.001)
+})
+
+
+# Tests for `getZone`
+
+test_that("`getZone` works with supported object classes", {
+  v <- sf::st_bbox(sf::st_as_sf(data.frame(x = c(-123.7835,-123.6352),
+                                           y = c(41.7534,41.8042)),
+                                coords = c("x", "y"),
+                                crs = 4326)) |>
+    sf::st_as_sfc()
+
+  p <- sf::st_as_sf(data.frame(x = c(-123.7835,-123.6352),
+                                           y = c(41.7534,41.8042)),
+                                coords = c("x", "y"),
+                                crs = 4326)
+
+  zone <- 3
+
+  # polygon
+  expect_equal(getZone(v), zone)
+  # points
+  expect_equal(getZone(p), zone)
+  # character string
+  expect_equal(getZone("Northern California Coastal Range"), zone)
+})
+
+
+test_that("`getZone` recognizes arguement errors", {
+  v <- sf::st_bbox(sf::st_as_sf(data.frame(x = c(-6.2,-6),
+                                           y = c(41.7534,41.8042)),
+                                coords = c("x", "y"),
+                                crs = 4326)) |>
+    sf::st_as_sfc()
+
+  # v_mult <- sf::st_bbox(sf::st_as_sf(data.frame(x = c(-114.77,-114.57),
+  #                                          y = c(33.26,33.37)),
+  #                               coords = c("x", "y"),
+  #                               crs = 4326)) |>
+  #   sf::st_as_sfc()
+
+  expect_error(getZone(v),
+               "argument `data` must be sf object or zone name within CONUS")
+
+  expect_error(getZone("Not a Zone"),
+               "argument `data` must be sf object or zone name within CONUS")
+
+  expect_error(getZone(terra::vect(v)),
+               "argument `data` must be sf object, or character string")
 })
