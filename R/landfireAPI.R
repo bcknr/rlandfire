1e1d9dfbde6e24af1cd9ffa290a31bcf9c54cc11
Mark Buckner <mab677@cornell.edu>
2025-03-17 12:46:05 -0600

    Depreciate landfireAPI() and set new name to landfireAPIv2().

--- R/landfireAPI.R
+++ R/landfireAPI.R
@@ -1,7 +1,7 @@
 #' Call the LANDFIRE Product Service (LFPS) API
 #'
 #' @description
-#' `landfireAPI` downloads LANDFIRE data by calling the LFPS API
+#' `landfireAPIv2` downloads LANDFIRE data by calling the LFPS API
 #'
 #' @param products Product names as character vector
 #'   (see: \href{https://lfps.usgs.gov/lfps/helpdocs/productstable.html}{Products Table})
@@ -43,14 +43,15 @@
 #' \dontrun{
 #' products <-  c("ASP2020", "ELEV2020", "230CC")
 #' aoi <- c("-123.7835", "41.7534", "-123.6352", "41.8042")
+#' email <- "email@@example.com>"
 #' projection <- 6414
 #' resolution <- 90
 #' edit_rule <- list(c("condition","ELEV2020","lt",500), c("change", "230CC", "st", 181))
 #' save_file <- tempfile(fileext = ".zip")
-#' resp <- landfireAPI(products, aoi, projection, resolution, edit_rule = edit_rule, path = save_file)
+#' resp <- landfireAPIv2(products, aoi, projection, resolution, edit_rule = edit_rule, path = save_file)
 #' }
 
-landfireAPI <- function(products, aoi, email, projection = NULL,
+landfireAPIv2 <- function(products, aoi, email, projection = NULL,
                         resolution = NULL, edit_rule = NULL, edit_mask = NULL,
                         priority_code = NULL, path = NULL, max_time = 10000,
                         method = "curl", verbose = TRUE) {
@@ -59,7 +60,7 @@ landfireAPI <- function(products, aoi, email, projection = NULL,
   # Missing
   stopifnot("argument `products` is missing with no default" = !missing(products))
   stopifnot("argument `aoi` is missing with no default" = !missing(aoi))
-  stopifnot("A valid `email` address is required. (See `?rlandfire::landfireAPI` for more information)" = grepl("@", email) && !missing(email))
+  stopifnot("A valid `email` address is required. (See `?rlandfire::landfireAPIv2` for more information)" = grepl("@", email) && !missing(email))
   stopifnot("argument `email` is missing with no default" = !missing(aoi))
 
   if(!is.null(edit_rule)){
@@ -308,4 +309,233 @@ landfireAPI <- function(products, aoi, email, projection = NULL,
          paste0(edit_rule[!is.na(edit_rule)], collapse = ','),
          '}]}'
   )
+}
+
+# Depreciated -----------------------------------------------------------
+
+
+#' Depreciated: Call the LANDFIRE Product Service (LFPS) API
+#'
+#' @description
+#' Depreciated: `landfireAPI()` is no longer supported due to updates to the 
+#' LFPS API. Use `landfireAPIv2()` instead.
+#' 
+#' `landfireAPI` downloads LANDFIRE data by calling the LFPS API
+#'
+#' @param products Product names as character vector
+#'   (see: \href{https://lfps.usgs.gov/lfps/helpdocs/productstable.html}{Products Table})
+#' @param aoi Area of interest as character or numeric vector defined by
+#'   latitude and longitude in decimal degrees in WGS84 and ordered
+#'   `xmin`, `ymin`, `xmax`, `ymax` or a LANDFIRE map zone.
+#' @param projection Optional. A numeric value of the WKID for the output projection
+#'    Default is a localized Albers projection.
+#' @param resolution Optional. A numeric value between 30-9999 specifying the
+#'   resample resolution in meters. Default is 30m.
+#' @param edit_rule Optional. A list of character vectors ordered "operator class"
+#'   "product", "operator", "value". Limited to fuel theme products only.
+#'   (see: \href{https://lfps.usgs.gov/lfps/helpdocs/LFProductsServiceUserGuide.pdf}{LFPS Guide})
+#' @param edit_mask Optional. **Not currently functional**
+#' @param path Path to `.zip` directory. Passed to [utils::download.file()].
+#'   If NULL, a temporary directory is created.
+#' @param max_time Maximum time, in seconds, to wait for job to be completed.
+#' @param method Passed to [utils::download.file()]. See `?download.file`
+#' @param verbose If FALSE suppress all status messages
+#'
+#' @return
+#' Returns a `landfire_api` object with named elements:
+#' * `request` - list with elements `query`, `date`, `url`, `job_id`,`dwl_url`
+#' * `content` - Informative messages passed from API
+#' * `response` - Full response
+#' * `status` - Final API status, one of "Failed", "Succeeded", or "Timed out"
+#' * `path` - path to save directory
+#'
+#' @md
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' products <-  c("ASP2020", "ELEV2020", "230CC")
+#' aoi <- c("-123.7835", "41.7534", "-123.6352", "41.8042")
+#' projection <- 6414
+#' resolution <- 90
+#' edit_rule <- list(c("condition","ELEV2020","lt",500), c("change", "230CC", "st", 181))
+#' save_file <- tempfile(fileext = ".zip")
+#' resp <- landfireAPI(products, aoi, projection, resolution, edit_rule = edit_rule, path = save_file)
+#' }
+
+landfireAPI <- function(products, aoi, projection = NULL, resolution = NULL,
+                        edit_rule = NULL, edit_mask = NULL, path = NULL,
+                        max_time = 10000, method = "curl", verbose = TRUE) {
+  
+  lifecycle::deprecate_warn(
+    "1.0.0",
+    "landfireAPI()",
+    "landfireAPIv2()",
+    c("The LANDFIRE Products Services API has been updated to v2 as of May 2025.",
+      "New parameters and syntax are required.",
+      "See `?rlandfire::landfireAPIv2` for more information.")
+  )
+
+  #### Checks
+  # Missing
+  stopifnot("argument `products` is missing with no default" = !missing(products))
+  stopifnot("argument `aoi` is missing with no default" = !missing(aoi))
+
+  if(!is.null(edit_rule)){
+    stopifnot("argument `edit_rule` must be a list" = inherits(edit_rule, "list"))
+
+    class <- sapply(edit_rule, `[`, 1)
+
+    stopifnot(
+      '`edit_rule` operator classes must only be "condition" or "change"' =
+        all(class %in% c("condition", "change"))
+    )
+
+    stopifnot(
+      '`edit_rule` conditional operators must be one of "eq","ge","gt","le","lt","ne"' =
+        all(sapply(edit_rule, `[`, 3)[class == "condition"] %in% c("eq","ge","gt","le","lt","ne")))
+
+    stopifnot(
+      '`edit_rule` change operators must be one of "cm","cv","cx","bd","ib","mb","st"' =
+        all(sapply(edit_rule, `[`, 3)[class == "change"] %in% c("cm","cv","cx","db","ib","mb","st")))
+  }
+
+  if(is.null(path)){
+    path = tempfile(fileext = ".zip")
+    warning("`path` is missing. Files will be saved in temp directory: ", path)
+  }
+
+  # Classes
+  stopifnot("argument `products` must be a character vector" = inherits(products, "character"))
+  stopifnot("argument `aoi` must be a character or numeric vector" = inherits(aoi, c("character", "numeric")))
+  stopifnot("argument `aoi` must be vector of coordinates with length == 4 or a single map zone" = length(aoi) == 1 | length(aoi) == 4)
+  stopifnot("argument `max_time` must be numeric" = inherits(max_time, c("numeric")))
+
+  stopifnot(
+    "`method` is invalid. See `?download.file`" =
+      method %in% c("internal", "libcurl", "wget", "curl", "wininet", "auto")
+  )
+
+  stopifnot("argument `verbose` must be logical" = inherits(verbose, "logical"))
+
+  stopifnot("argument `max_time` must be >= 5" = max_time >= 5)
+
+  #check for special characters in edit mask
+  # grepl('[^[:alnum:]]', val)
+  # Check it is sf or spatvector
+
+  # Values in range
+  if(is.numeric(resolution) && resolution == 30) {
+    resolution <- NULL
+  }
+
+  if(!is.null(resolution) && !all(resolution >= 30 & resolution <= 9999)) {
+    stop("argument `resolution` must be between 30 and 9999 or `NULL`")
+  }
+
+  aoi <- as.numeric(aoi)
+
+  if(length(aoi) == 4) {
+    # Likely lat/lon?
+    if(!all(aoi[c(1,3)] >=-180 & aoi[c(1,3)] <=180 &
+            aoi[c(2,4)] >=-90 & aoi[c(2,4)] <=90)){
+      stop("argument `aoi` must be latitude and longitude in decimal degrees (WGS84) or a LANDFIRE map zone")
+    }
+    # Correct order?
+    if(!all(aoi[[1]] < aoi[[3]] & aoi[[2]] < aoi[[4]])) {
+      stop("argument `aoi` must be ordered `xmin`, `ymin`, `xmax`, `ymax`")
+    }
+    # Valid map zone?
+  } else if(length(aoi) == 1 & !all(aoi >= 1 & aoi <= 79)){
+    stop("argument `aoi` must be between 1 and 79 when using LANDFIRE map zones")
+  }
+
+  #### End Checks
+
+  if(!is.null(edit_rule)) {
+    edit_rule <- .fmt_editrules(edit_rule)
+  }
+
+  base_url <- httr::parse_url("https://lfps.usgs.gov/arcgis/rest/services/LandfireProductService/GPServer/LandfireProductService/submitJob?")
+  base_url$query <- list(Layer_List = paste(products, collapse = ";"),
+                         Area_of_Interest = paste(aoi, collapse = " "),
+                         Output_Projection = projection,
+                         Resample_Resolution = resolution,
+                         Edit_Rule = edit_rule #,
+                         #Edit_Mask = edit_mask
+  )
+
+  url <- httr::build_url(base_url)
+  r <- httr::GET(url)
+  job_id <- sub(".*jobs/(.*)$", "\\1", r$url)
+  dwl_url <- paste0("https://lfps.usgs.gov/arcgis/rest/directories/arcgisjobs/landfireproductservice_gpserver/",
+                    job_id, "/scratch/", job_id, ".zip")
+
+  # Loop through up to max time
+  mt <- max_time*10
+
+  for (i in 1:mt) {
+    # Check status
+    r <- httr::GET(r$url)
+    status <- httr::status_code(r) #API always returns a successful status code (200)
+    content <- strsplit(httr::content(r, "text"), "\r\n")[[1]]
+
+    # Parse content for messaging and error reporting
+    message <- gsub("\\<.*?\\>", "", content, perl = TRUE)
+    job_status <- message[grep("Job Status", message)]
+    inf_msg <- message[grep("esriJobMessageType", message)]
+
+    if(verbose == TRUE) {
+      # There is a better way to do this but for now...clear console each loop:
+      cat("\014")
+
+      cat(job_status,"\nJob Messages:\n",paste(inf_msg, collapse = "\n"),
+          "\n-------------------",
+          "\nElapsed time: ", sprintf("%.1f", round(i*0.1, 1)), "s", "(Max time:", max_time, "s)",
+          "\n-------------------\n")
+    }
+
+    # If failed exit and report
+    if(status != 200 | grepl("Failed",job_status)) {
+      warning(job_status)
+      status <- "Failed"
+      break
+
+      # If success report success and download file
+    } else if(grepl("Succeeded",job_status)) {
+      utils::download.file(dwl_url, path, method = method, quiet = !verbose)
+      status <- "Succeeded"
+      break
+
+      # Print current status, wait, and check again
+    } else {
+      Sys.sleep(0.1)
+    }
+
+    # Max time error
+    if(i == mt) {
+      cat("\n")
+      warning("Job status: Incomplete and max_time reached\nVisit URL to check status and download manually:\n   ", r$url)
+      status <- "Timed out"
+      break
+    }
+
+  }
+
+  # construct landfire_api object
+  structure(
+    list(
+      request = list(query = base_url$query,
+                     date = Sys.time(),
+                     url = url,
+                     job_id = job_id,
+                     dwl_url = dwl_url),
+      content = inf_msg, # currently just returns a vector
+      response = r,
+      status = status,
+      path = path
+    ),
+    class = "landfire_api"
+  )
+
 }
--- tests/testthat/test-landfireAPI.R
+++ tests/testthat/test-landfireAPI.R
@@ -1,6 +1,6 @@
-# Tests for landfireAPI.R
+# Tests for landfireAPIv2.R
 
-test_that("`landfireAPI()` recognizes arguement errors", {
+test_that("`landfireAPIv2()` recognizes arguement errors", {
   products <-  c("ASP2020", "ELEV2020", "230CC")
   aoi <- c("-123.7835", "41.7534", "-123.6352", "41.8042")
   email <- "mab677@cornell.edu"
@@ -11,89 +11,89 @@ test_that("`landfireAPI()` recognizes arguement errors", {
   path <- tempfile(fileext = ".zip")
 
   # Check for required arguments
-  expect_error(landfireAPI(aoi = aoi, email = email),
+  expect_error(landfireAPIv2(aoi = aoi, email = email),
                "argument `products` is missing with no default")
 
-  expect_error(landfireAPI(products, email = email),
+  expect_error(landfireAPIv2(products, email = email),
                "argument `aoi` is missing with no default")
 
-  expect_error(landfireAPI(products, aoi),
+  expect_error(landfireAPIv2(products, aoi),
                'argument "email" is missing, with no default')
 
   # Check class
-  expect_error(landfireAPI(products = c(1,2,3), aoi,
+  expect_error(landfireAPIv2(products = c(1,2,3), aoi,
                            email = email, path = path),
                "argument `products` must be a character vector")
   
-  expect_error(landfireAPI(products, aoi = list(1,2,3),
+  expect_error(landfireAPIv2(products, aoi = list(1,2,3),
                            email = email, path = path),
                "argument `aoi` must be a character or numeric vector")
   
-  expect_error(landfireAPI(products, aoi, email = "notanemail"),
-               "A valid `email` address is required. (See `?rlandfire::landfireAPI` for more information)",
+  expect_error(landfireAPIv2(products, aoi, email = "notanemail"),
+               "A valid `email` address is required. (See `?rlandfire::landfireAPIv2` for more information)",
                fixed = TRUE)
   
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            max_time = TRUE, path = path),
                "argument `max_time` must be numeric")
   
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            verbose = "yes", path = path),
                "argument `verbose` must be logical")
   
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            edit_rule = "edit_rule"),
                "argument `edit_rule` must be a list")
 
   # Check `aoi` errors
-  expect_error(landfireAPI(products, aoi = 100, email = email, path = path),
+  expect_error(landfireAPIv2(products, aoi = 100, email = email, path = path),
                "argument `aoi` must be between 1 and 79 when using LANDFIRE map zones")
 
-  expect_error(landfireAPI(products, aoi = c(-200, 43, -179, 44),
+  expect_error(landfireAPIv2(products, aoi = c(-200, 43, -179, 44),
                            email = email, path = path),
                "argument `aoi` must be latitude and longitude in decimal degrees (WGS84) or a LANDFIRE map zone",
                fixed = TRUE)
   
-  expect_error(landfireAPI(products, aoi = c(-123, 43, -124, 44),
+  expect_error(landfireAPIv2(products, aoi = c(-123, 43, -124, 44),
                            email = email, path = path),
                "argument `aoi` must be ordered `xmin`, `ymin`, `xmax`, `ymax`")
   
-  expect_error(landfireAPI(products, aoi = c(65,66),
+  expect_error(landfireAPIv2(products, aoi = c(65,66),
                            email = email, path = path),
                "argument `aoi` must be vector of coordinates with length == 4 or a single map zone")
 
   # Check `resolution`
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            resolution = 20, path = path),
                "argument `resolution` must be between 30 and 9999 or `NULL`")
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            resolution = 10000, path = path),
                "argument `resolution` must be between 30 and 9999 or `NULL`")
 
   # Check edit_rule arguments
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            edit_rule = list(c("wrong","ELEV2020","lt",500),
                                             c("change", "230CC", "st", 181))),
                '`edit_rule` operator classes must only be "condition" or "change"')
-  expect_error(landfireAPI(products, aoi, email = email,
+  expect_error(landfireAPIv2(products, aoi, email = email,
                            edit_rule = list(c("condition","ELEV2020","xx",500),
                                             c("change", "230CC", "st", 181))),
                '`edit_rule` conditional operators must be one of "eq","ge","gt","le","lt","ne"')
 })
 
-test_that("`landfireAPI()` returns helpful errors with email/priority_code", {
+test_that("`landfireAPIv2()` returns helpful errors with email/priority_code", {
 
   products <-  c("ASP2020")
   aoi <- c("-123.7835", "41.7534", "-123.6352", "41.8042")
   projection <- 6414
 
   # ID errors with LFPSv1 requests with positional arguments
-  expect_error(landfireAPI(products, aoi, projection),
-               "A valid `email` address is required. (See `?rlandfire::landfireAPI` for more information)",
+  expect_error(landfireAPIv2(products, aoi, projection),
+               "A valid `email` address is required. (See `?rlandfire::landfireAPIv2` for more information)",
                fixed = TRUE)
 
   # Check class
-  expect_error(landfireAPI(products, aoi, email = "test@email.com",
+  expect_error(landfireAPIv2(products, aoi, email = "test@email.com",
                            priority_code = 1),
                "argument `priority_code` must be a character string")
 
@@ -102,7 +102,7 @@ test_that("`landfireAPI()` returns helpful errors with email/priority_code", {
 # TODO: Check that URL is built correctly with email and priority_code
 
 
-test_that("`landfireAPI()` recognizes failed call", {
+test_that("`landfireAPIv2()` recognizes failed call", {
 
   skip_on_cran()
 
@@ -112,11 +112,11 @@ test_that("`landfireAPI()` recognizes failed call", {
   projection <- 123456
   path <- tempfile(fileext = ".zip")
 
-  expect_warning(landfireAPI(products, aoi, email, projection, path = path),
+  expect_warning(landfireAPIv2(products, aoi, email, projection, path = path),
                  "Job Status:  esriJobFailed")
 })
 
-test_that("`landfireAPI()` edge cases", {
+test_that("`landfireAPIv2()` edge cases", {
 
   skip_on_cran()
 
@@ -127,12 +127,12 @@ test_that("`landfireAPI()` edge cases", {
   path <- tempfile(fileext = ".zip")
 
   # Functions when resolution = 30
-  expect_no_error(landfireAPI(products, aoi, email,
+  expect_no_error(landfireAPIv2(products, aoi, email,
                               resolution = 30, path = path))
 })
 
 
-test_that("`landfireAPI()` works with `getAOI()` and `getZone()`", {
+test_that("`landfireAPIv2()` works with `getAOI()` and `getZone()`", {
 
   skip_on_cran()
 
@@ -150,9 +150,9 @@ test_that("`landfireAPI()` works with `getAOI()` and `getZone()`", {
   aoi <- getAOI(r)
   zone <- getZone("Northern California Coastal Range")
 
-  expect_no_error(landfireAPI(products = products, aoi = aoi, email = email,
+  expect_no_error(landfireAPIv2(products = products, aoi = aoi, email = email,
                               resolution = resolution, path = path))
-  expect_no_error(landfireAPI(products = products, aoi = zone, email = email,
+  expect_no_error(landfireAPIv2(products = products, aoi = zone, email = email,
                               resolution = resolution, path = path))
 })
 
