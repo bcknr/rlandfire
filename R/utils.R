c789b1399132f02285232cc383f201a2e47e916a
Mark Buckner <mab677@cornell.edu>
2025-03-21 09:30:40 -0600

    Added internal constructor for landfire_api class

--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,44 @@
 viewProducts <- function() {
   utils::browseURL("https://lfps.usgs.gov/lfps/helpdocs/productstable.html")
 }
+
+
+#' Internal: Build Landfire API object
+#'
+#' @param params List of parameters to be passed to the API
+#' @param request httr2_request object
+#' @param init_resp httr2_response object for the initial request
+#' @param job_id Job ID
+#' @param dwl_url URL to download the file
+#' @param content Content of the response
+#' @param response httr2_response object for the final request
+#' @param status Status of the request
+#' @param path Path to the file
+#'
+#' @return Character vector (length = 1) with formated edit rules call
+#'
+#' @noRd
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' }
+.build_landfire_api <- function(params = NULL, request = NULL, init_resp = NULL,
+                                job_id = NULL, dwl_url = NULL, content = NULL,
+                                response = NULL, status = NULL, path = NULL) {
+  structure(
+    list(
+      request = list(query = params,
+                     date = Sys.time(),
+                     url = ifelse(is.null(request), NULL, request$url),
+                     job_id = job_id,
+                     request = init_resp,
+                     dwl_url = dwl_url),
+      content = content,
+      response = response,
+      status = status,
+      path = path
+    ),
+    class = "landfire_api"
+  )
+}
