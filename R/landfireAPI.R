d0d8d413453341f0fd3b089f45c14c96e2d93cde
Mark Buckner <mab677@cornell.edu>
2025-04-19 21:39:00 -0600

    Minor bug fix

--- R/landfireAPI.R
+++ R/landfireAPI.R
@@ -287,7 +287,9 @@ landfireAPIv2 <- function(products, aoi, email, projection = NULL,
   }
 
   return(list(item_id = sprintf('[{"itemID":"%s"}]', upload_body$itemId),
-              item_name = gsub("\\.zip$", ".shp", upload_body$itemName)))
+              item_name = grep("\\.shp$",
+                               unzip(file, list=T)$Name,
+                               value = TRUE)))
 
 }
 
