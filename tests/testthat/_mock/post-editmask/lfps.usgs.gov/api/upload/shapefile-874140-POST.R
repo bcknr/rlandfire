fb26a6aa5b93d466d1fb8115f5a49581baf32a21
Mark Buckner <mab677@cornell.edu>
2025-07-22 12:58:21 -0600

    Drop mock API call for `.post_editmask` and just skip on CRAN

--- tests/testthat/_mock/post-editmask/lfps.usgs.gov/api/upload/shapefile-874140-POST.R
+++
@@ -1,8 +0,0 @@
-structure(list(method = "POST", url = "https://lfps.usgs.gov/api/upload/shapefile", 
-    status_code = 500L, headers = structure(list(date = "Tue, 22 Jul 2025 15:39:12 GMT", 
-        server = "Apache", `x-frame-options` = "SAMEORIGIN", 
-        `strict-transport-security` = "max-age=31536000; includeSubDomains", 
-        vary = "Accept-Encoding", `content-encoding` = "br", 
-        `access-control-allow-origin` = "*", `content-length` = "1", 
-        `Set-Cookie` = "REDACTED", `Set-Cookie` = "REDACTED"), redact = character(0), class = "httr2_headers"), 
-    body = raw(0), cache = new.env(parent = emptyenv())), class = "httr2_response")
